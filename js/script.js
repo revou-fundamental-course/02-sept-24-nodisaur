cddfa78754f241d168350eff96f36310e6f197ce
kila <aqiilaputriza@gmail.com>
2024-09-12 18:19:13 +0700

    update js dan html

--- js/script.js
+++ js/script.js
@@ -2,8 +2,17 @@ function validateLuas() {
     let panjangAlas = document.getElementById('panjang-alas').value;
     let tinggi = document.getElementById('tinggi').value;
     if (panjangAlas == '' || tinggi =='' || panjangAlas.includes('e') || tinggi.includes('e')) {
-        alert('Isi nilai');
+        alert('Isi nilai dengan benar');
     }
-    console.log(panjangAlas);
-    console.log(tinggi);
+    let luas = 0.5 * panjangAlas * tinggi;
+    document.getElementById('hasil-1').value = luas;
+}
+
+function validateKeliling() {
+    let panjangSisi = document.getElementById('panjang-sisi').value;
+    if (panjangSisi == '' || panjangSisi.includes('e')) {
+        alert('Isi nilai dengan benar');
+    }
+    let keliling = 3 * panjangSisi;
+    document.getElementById('hasil-2').value = keliling;
 }
